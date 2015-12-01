77332d5cd0d9ec58817deb158b7692f6f8c10ce6
andrerpena <andrerpena@gmail.com>
2015-11-30 22:45:02 -0200

    Button for clearing the search now available

--- src/components/VNav.js
+++ src/components/VNav.js
@@ -1,4 +1,5 @@
-var React = require("react");
+import React from "react";
+import ReactDOM from 'react-dom';
 import update from 'react-addons-update';
 import VNavItem from './VNavItem';
 import Icon from './Icon';
@@ -7,14 +8,27 @@ import clone from 'clone';
 
 var VNav = React.createClass({
 
-    getInitialState: function() {
+    getDefaultProps: function () {
+        return {
+            clearSearchText: "Clear search"
+        }
+    },
+
+
+    getInitialState: function () {
         return {
             search: null
         }
     },
 
-    handleTextChange: function(event) {
+    handleTextChange: function (event) {
         this.setState({search: event.target.value});
+    },
+
+    handlerClearSearch: function(event) {
+        this.setState({search: null}, function() {
+            ReactDOM.findDOMNode(this.refs.searchInput).focus();
+        });
     },
 
     /**
@@ -22,16 +36,28 @@ var VNav = React.createClass({
      * @returns {XML}
      */
     render: function () {
+
         let nodesClone = clone(this.props.nodes);
         let nodes = this.state.search ? menuHelper.filterNodes(nodesClone, this.state.search) : nodesClone;
+
+        let searchClearer = this.state.search ? <div className="search-clearer">
+            {this.props.clearSearchText}
+            <div className="search-clearer-button pull-right" onClick={this.handlerClearSearch} >
+                <i className="fa fa-close" ></i>
+            </div>
+        </div> : null;
+
         return <div className="vnav">
             <div className="search-wrapper">
                 <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Filter" onChange={this.handleTextChange}/>
+                    <input ref="searchInput" type="text" className="form-control" placeholder="Filter" onChange={this.handleTextChange} value={this.state.search}/>
                     <span className="input-group-addon"><Icon icon="search"/></span>
                     <span className="input-group-addon"><Icon icon="gear"/></span>
                 </div>
 
+            </div>
+            <div className="search-clearer-wrapper">
+                {searchClearer}
             </div>
             <div className="children-wrapper">
                 {menuHelper.createVNavItemsFromNodes(nodes)}
